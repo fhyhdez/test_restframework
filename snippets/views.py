48d7ac09eeffba396aaeac4b101e611697710d67
fatima <fa.hikariangel@gmail.com>
2018-05-23 00:45:32 -0500

    routers hace todo :v

--- snippets/views.py
+++ snippets/views.py
@@ -1,41 +1,30 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
-from rest_framework import generics
 from django.contrib.auth.models import User
-from snippets.serializers import UserSerializer
-from rest_framework import permissions
-from snippets.permissions import IsOwnerOrReadOnly
-from rest_framework.decorators import api_view
-from rest_framework.reverse import reverse
-from rest_framework import renderers
-from rest_framework import viewsets
-from rest_framework.decorators import action
+from rest_framework import generics, permissions, renderers, viewsets
+from rest_framework.decorators import api_view, detail_route
 from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
+from snippets.models import Snippet
+from snippets.permissions import IsOwnerOrReadOnly
+from snippets.serializers import SnippetSerializer, UserSerializer
 
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    })
 
 class SnippetViewSet(viewsets.ModelViewSet):
     """
     This viewset automatically provides `list`, `create`, `retrieve`,
     `update` and `destroy` actions.
-
     Additionally we also provide an extra `highlight` action.
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-                          IsOwnerOrReadOnly,)
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly, )
 
-    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
     def highlight(self, request, *args, **kwargs):
         snippet = self.get_object()
         return Response(snippet.highlighted)
